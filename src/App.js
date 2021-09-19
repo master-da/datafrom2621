09425941a1d94e01e1605e40adce03011618250a
master-da <mdnokib2000@gmail.com>
2021-09-19 18:06:35 +0600

    eyy

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React, {useState} from "react";
 import { LineChart, Line, CartesianGrid, XAxis, Tooltip, YAxis } from "recharts";
 import './App.css';
 
-const cors = "https://cors-anywhere.herokuapp.com/";
-const url = "http://r.applovin.com/maxReport"
-const api_key = "api_key=U_6ufDXDPxfXT5mJr1TXCfBDawPb6mmr3W01UHfLA6tC5gS_R-aTMng9oG4vXLk7wDJL8H_UKPGL3QtereTazI";
-const format = "format=JSON";
-const filter = "filter_application=Shark Attack,Shark Attack 3D"
-const sort = "sort_day=ASC&sort&hour=ASC"
-const columns = "columns=day,hour,application,estimated_revenue,ecpm";
-const start = "start=2021-09-09";
-const end = "end=2021-09-16";
 
 function App() {
 
+  const cors = "https://cors-anywhere.herokuapp.com/";
+  const url = "http://r.applovin.com/maxReport"
+  const api_key = "api_key=U_6ufDXDPxfXT5mJr1TXCfBDawPb6mmr3W01UHfLA6tC5gS_R-aTMng9oG4vXLk7wDJL8H_UKPGL3QtereTazI";
+  const format = "format=JSON";
+  const filter = "filter_application=Shark Attack,Shark Attack 3D"
+  const sort = "sort_day=ASC&sort&hour=ASC"
+  const columns = "columns=day,hour,application,estimated_revenue,ecpm";
+  // const start = "start=2021-09-09";
+  const end = "end=2021-09-16";
+
   const [data, setData] = useState([]);
+  const [start, setStart] = useState("start=2021-09-09");
 
   const reqd = async () => {
     const axios = require('axios');
@@ -25,20 +27,33 @@ function App() {
         console.log(error);
       }
   }
+
+  const changerman = () =>{
+
+  }
   
   return (
     <div>
       <div>
-        <button onClick = {reqd}>data = got</button>
+      <div>
+
+        <input type="radio" value="day" name="day"/> 1 Week
+        <input type="radio" value="week" name="week"/> 1 Day
+        <input type="radio" value="month" name="month"/> 1 Month
+      
+      </div>
+
+        <button onClick = {reqd}>refresh</button>
+
         <LineChart width={1600} height={900} data={data}>
-        
+
             <Line type="monotone" dataKey="ecpm" yAxisId={0}/>
-            <Line type="monotone" dataKey="estimated_revenue" yAxisId={1}/>
-
-            <XAxis dataKey="day"/>
-
             <YAxis yAxisId={0} type="number" domain={[0, 35]}/>
+
+            <Line type="monotone" dataKey="estimated_revenue" yAxisId={1}/>
             <YAxis yAxisId={1} type="number" domain={[0, 0.5]} orientation='right'/>
+            
+            <XAxis dataKey="day"/>
 
             <CartesianGrid/>
             <Tooltip/>
